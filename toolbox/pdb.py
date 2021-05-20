0058c0cffc3aa141d450ef1a96e521e0c6bda8f1
Evžen Wybitul <wybitul.evzen@gmail.com>
2021-05-20 12:24:24 +0200

    Add residue exposure

--- toolbox/pdb.py
+++ toolbox/pdb.py
@@ -1,5 +1,8 @@
+from typing import Counter
 import Bio.PDB as P
+from Bio.PDB.ResidueDepth import get_surface, min_dist
 from scipy.spatial.distance import pdist
+from numpy import isclose
 
 
 class Structure:
@@ -37,3 +40,42 @@ class Structure:
     def search_around(self, ligand, radius: float, level: str):
         ns = P.NeighborSearch(P.Selection.unfold_entities(self.structure, "A"))
         return ns.search(ligand.get_coord(), radius, level)
+
+    # Not tested, the msms tool doesn't run on my system
+    def get_residue_exposure(self):
+        exposed = Counter()
+        buried = Counter()
+        surface = get_surface(self.structure)
+        for r in self.structure.get_residues():
+            aa = r.get_resname()
+            mindist = min(min_dist(a.get_coord(), surface) for a in r.get_atoms())
+            if isclose(mindist, 0):
+                exposed[aa] += 1
+            else:
+                buried[aa] += 1
+        total = sum(buried.values()) + sum(exposed.values())
+        return {
+            "buried": {
+                "ratio": sum(buried.values()) / total,
+                "distribution": list(buried.items()),
+            },
+            "exposed": {
+                "ratio": sum(exposed.values()) / total,
+                "distribution": list(exposed.items()),
+            },
+        }
+
+    def get_polarity(self):
+        """
+        :return: ratio of polar to nonpolar aminoacids that are buried or exposed
+        """
+
+        def get_ratio(amk_type):
+            amk_pol = sum(num for amk, num in amk_type.items() if amk in polar_amks)
+            return amk_pol / sum(num for num in amk_type.values())
+
+        dic = self.get_buried_exposed_by_amk()
+        buried, exposed = dic["buried"], dic["exposed"]
+        buried_pol_ratio = get_ratio(buried)
+        exposed_pol_ratio = get_ratio(exposed)
+        return {"buried ratio": buried_pol_ratio, "exposed ratio": exposed_pol_ratio}
